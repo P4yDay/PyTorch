2f490231c21b4528bb145a3f45d29f46d65e909b
P4yDay <46981551+P4yDay@users.noreply.github.com>
2019-12-11 15:57:25 -0600

    working on handwriting case use

--- PyTorchStuff/HandwrittenDigClassification.py
+++ PyTorchStuff/HandwrittenDigClassification.py
@@ -10,4 +10,18 @@ _task = transforms.Compose([
 ])
 
 ## LOAD MNIST DATASET AND APPLY TRANSFORMATION
- mnist = MNIST("data", download=True, train=True, transform=_task)
+mnist = MNIST("data", download = True, train = True, transform =_task)
+
+## CREATE TRAINING AND VALIDATION SPLIT
+split = int(0.8 * len(mnist))
+index_list = list(range(len(mnist)))
+train_idx, valid_idx = index_list[:split], index_list[split:]
+
+## create sampler objects using SubsetRandomSampler
+tr_sampler = SubsetRandomSampler(train_idx)
+val_sampler = SubsetRandomSampler(valid_idx)
+
+## Create iterator objects for train and valid datasets
+trainloader = DataLoader(mnist, batch_size = 256, sampler = tr_sampler)
+validloader = DataLoader(mnist, batch_size = 256, sampler = val_sampler)
+
