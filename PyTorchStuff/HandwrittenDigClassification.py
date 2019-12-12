6f32238318b07562dcb5cf8c9c18e6d96c769674
P4yDay <46981551+P4yDay@users.noreply.github.com>
2019-12-12 09:48:12 -0600

    finishing handwriting

--- PyTorchStuff/HandwrittenDigClassification.py
+++ PyTorchStuff/HandwrittenDigClassification.py
@@ -2,7 +2,9 @@ from torchvision import transforms
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
-import torch.nn.functional as F 
+import torch.nn.functional as F
+from torch import optim
+
 ## 1ST TRANSFORMATION CONVERTS THE RAW DATA INTO TENSOR VARIABLES 
 # AND THE SECOND TRANSFORMATION PERFORMS NORMALIZATION
 _task = transforms.Compose([
@@ -26,3 +28,18 @@ val_sampler = SubsetRandomSampler(valid_idx)
 trainloader = DataLoader(mnist, batch_size = 256, sampler = tr_sampler)
 validloader = DataLoader(mnist, batch_size = 256, sampler = val_sampler)
 
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden = nn.Linear(784, 128)
+        self.output = nn.Linear(128, 10)
+    
+    def forward(self, x):
+        x = self.hidden(x)
+        x = F.sigmoid(x)
+        x = self.output(x)
+        return x
+model = Model()
+
+# Define the loss function and the optimizer using nn and optim package
+loss_function = 
