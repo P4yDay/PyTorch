74c21362dd9c0e86359af349ca534ab222448641
P4yDay <46981551+P4yDay@users.noreply.github.com>
2019-12-12 09:29:17 -0600

    finishing handwriting

--- PyTorchStuff/HandwrittenDigClassification.py
+++ PyTorchStuff/HandwrittenDigClassification.py
@@ -2,6 +2,7 @@ from torchvision import transforms
 from torchvision.datasets import MNIST
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
+import torch.nn.functional as F 
 ## 1ST TRANSFORMATION CONVERTS THE RAW DATA INTO TENSOR VARIABLES 
 # AND THE SECOND TRANSFORMATION PERFORMS NORMALIZATION
 _task = transforms.Compose([
